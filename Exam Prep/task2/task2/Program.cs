950314f643f121f3db984c603bf86ee6695cce7a
bogi <bogity123@gmail.com>
2022-03-24 15:08:48 +0200

    Added task2

---
+++ Exam Prep/task2/task2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //02. Fancy Barcodes
+
+            int n = int.Parse(Console.ReadLine());
+
+            Regex pattern = new Regex(@"@#+([A-Z][A-Za-z0-9]{4,}[A-Z])@#+");            
+
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+                if (pattern.IsMatch(input))
+                {
+                    string number = string.Empty;
+                    bool isFound = false;
+                    foreach (var c in input)
+                    {
+                        if (Char.IsDigit(c))
+                        {
+                            number += c;
+                            isFound = true;
+                        }
+                    }
+                    if (!isFound)
+                    {
+                        Console.WriteLine($"Product group: 00");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Product group: {number}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid barcode");
+                }
+            }
+
+        }
+    }
+}
