22a28e49c9d44b35cb83666ba54cc5bb69737d8a
bogi <bogity123@gmail.com>
2022-03-29 22:57:54 +0300

    added 1 more task

---
+++ Exam Prep/Ad Astra/Ad Astra/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Ad_Astra
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"(\#|\|)(?<name>[A-Za-z\s]+)\1(?<date>[0-9]{2}\/[0-9]{2}\/[0-9]{2})\1(?<calories>[0-9]{1,4}|10000)\1";
+            Regex regex = new Regex(pattern);
+
+            string input = Console.ReadLine();
+
+            MatchCollection matches = regex.Matches(input);
+
+            int sum = 0;
+
+            foreach (Match match in matches)
+            {
+                int currCalories = int.Parse(match.Groups["calories"].Value);
+                sum += currCalories;
+            }
+
+            int days = sum / 2000;
+            Console.WriteLine($"You have food to last you for: {days} days!");
+
+            foreach (Match match in matches)
+            {
+                Console.WriteLine($"Item: {match.Groups["name"].Value}, Best before: {match.Groups["date"].Value}, Nutrition: {match.Groups["calories"].Value}");
+            }
+        }
+    }
+}
